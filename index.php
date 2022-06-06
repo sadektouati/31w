3060d26ed3c342f8db904c0f987b40917b88a942
Touati <e2195836@cmaisonneuve.qc.ca>
2022-06-06 17:35:10 -0400

    créé les deux fichiers

--- index.php
+++ index.php
@@ -1,20 +1,9 @@
 <?php get_header(); ?> 
-<h1>Mon premier modèle Wordpress</h1>
+<h1>index.php Mon premier modèle Wordpress</h1>
 <?php
 if (have_posts()):
     while(have_posts()) : the_post(); ?>
         <h2><?php the_title(); ?></h2>
-        <p><?php the_content() ?></p>
-
-<?php
-    $contenu = get_the_content();
-    echo wp_trim_words($contenu, 20, "...");
-?>
-
-<p>
-    <?= wp_trim_words(get_the_content(), 20, "<a href='".get_the_permalink()."' class=button>" . substr(get_the_title(), 0, 8) . "</a>") ?>
-</p>
-
     <?php endwhile; ?>
 <?php endif; ?>    
 
---
+++ single.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?> 
+<h1>single.php Mon premier modèle Wordpress</h1>
+<?php
+if (have_posts()):
+    while(have_posts()) : the_post(); ?>
+        <h2><?php the_title(); ?></h2>
+        <p><?php the_content() ?></p>
+
+<?php
+    $contenu = get_the_content();
+    echo wp_trim_words($contenu, 20, "...");
+?>
+
+<p>
+    <?= wp_trim_words(get_the_content(), 20, "<a href='".get_the_permalink()."' class=button>" . substr(get_the_title(), 0, 8) . "</a>") ?>
+</p>
+
+    <?php endwhile; ?>
+<?php endif; ?>    
+
+<?php get_footer(); ?>

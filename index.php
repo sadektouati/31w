68f19a9e466e3f9a391881eee0c53c997021c2f6
Touati <e2195836@cmaisonneuve.qc.ca>
2022-06-06 16:20:36 -0400

    replacer the_excerpt par the_content()

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <?php
 if (have_posts()):
     while(have_posts()) : the_post(); ?>
-        <p><?php the_title(); ?></p>
-        <p><?php the_excerpt() ?></p>
+        <h2><?php the_title(); ?></h2>
+        <p><?php the_content() ?></p>
     <?php endwhile; ?>
 <?php endif; ?>    
 
